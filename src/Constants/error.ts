aac5c6c3ccb4fbff7a9feda0578a0ea71246fa34
mateuse <emateus71@gmail.com>
2024-08-29 23:26:28 -0400

    add error message

--- src/Constants/error.ts
+++ src/Constants/error.ts
@@ -5,5 +5,6 @@ export const errorMessages = {
     FAILED_EXERCISE_RETRIEVAL_ERROR: "Failed to retrieve exercises",
     NO_EXERCISE_WORKOUT_DAY: "Please add an exercise to the workout day",
     NO_WORKOUT_DAY_WORKOUT: "Please add at least one workout day to the workout",
-    NO_DAYS_WORKOUT: "Please add at least one day to the workout"
+    NO_DAYS_WORKOUT: "Please add at least one day to the workout",
+    NO_WORKOUT_IN_DAY: "Please add a workout or rest day to the workout day"
 }
